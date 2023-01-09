e4e5063823ed35568c916a70d67b985d6d113e09
Indraswara <indrswara@gmail.com>
2023-01-09 10:19:05 +0700

    Health System

---
+++ Assets/Script/EnemyHealth.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int maxHealth = 10; 
+    public int health; 
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    public void TakeDamage(int damage)
+    {
+        health -= damage; 
+        
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
