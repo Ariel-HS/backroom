4460e2b6924fa22ef097d64a8a07a6195994aedf
Ariel-HS <arielherfrison@gmail.com>
2023-01-11 19:10:45 +0700

    changed health counter to health bar

--- Assets/Script/UIDisplay.cs
+++ Assets/Script/UIDisplay.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class UIDisplay : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI healthText;
+    ///[SerializeField] TextMeshProUGUI healthText;
     [SerializeField] TextMeshProUGUI coinText;
     PointKeeper pointKeeper;
+
+    public Slider healthSlider;
 
     void Awake()
     {
@@ -16,13 +19,16 @@ public class UIDisplay : MonoBehaviour
 
     void Start()
     {
-        healthText.text = "Health: " + pointKeeper.GetHealth();
+        ///healthText.text = "Health: " + pointKeeper.GetHealth();
+        healthSlider.maxValue = pointKeeper.GetHealth();
+        healthSlider.value = pointKeeper.GetHealth();
         coinText.text = "Coins: " + pointKeeper.GetCoin();
     }
 
     void Update()
     {
-        healthText.text = "Health: " + pointKeeper.GetHealth();
+        ///healthText.text = "Health: " + pointKeeper.GetHealth();
+        healthSlider.value = pointKeeper.GetHealth();
         coinText.text = "Coins: " + pointKeeper.GetCoin();
     }
 }
